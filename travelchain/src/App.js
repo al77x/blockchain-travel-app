a6815cb67436abee56d7e9c06b2ec1df98ee01d8
tuna <supermoonx86@gmail.com>
2024-10-11 10:54:58 +1300

    add change network function

--- travelchain/src/App.js
+++ travelchain/src/App.js
@@ -6,27 +6,95 @@ const CONTRACT_ADDRESS = '0x95fe5cc04A1553882A762Bc576094596B9e63756';
 const CONTRACT_ABI = [
   {
     "inputs": [],
-    "name": "owner",
-    "outputs": [
-      { "internalType": "address", "name": "", "type": "address" }
-    ],
-    "stateMutability": "view",
-    "type": "function"
+    "stateMutability": "nonpayable",
+    "type": "constructor"
   },
   {
-    "inputs": [{ "internalType": "address", "name": "", "type": "address" }],
-    "name": "users",
-    "outputs": [
-      { "internalType": "uint256", "name": "points", "type": "uint256" }
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "user",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "internalType": "address",
+        "name": "service",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "amount",
+        "type": "uint256"
+      }
     ],
-    "stateMutability": "view",
-    "type": "function"
+    "name": "PointsIssued",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "user",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "amount",
+        "type": "uint256"
+      }
+    ],
+    "name": "PointsRedeemed",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "from",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "internalType": "address",
+        "name": "to",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "amount",
+        "type": "uint256"
+      }
+    ],
+    "name": "PointsTransferred",
+    "type": "event"
   },
   {
     "inputs": [
-      { "internalType": "address", "name": "user", "type": "address" },
-      { "internalType": "uint256", "name": "points", "type": "uint256" },
-      { "internalType": "address", "name": "serviceProvider", "type": "address" }
+      {
+        "internalType": "address",
+        "name": "user",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "points",
+        "type": "uint256"
+      },
+      {
+        "internalType": "address",
+        "name": "serviceProvider",
+        "type": "address"
+      }
     ],
     "name": "issuePoints",
     "outputs": [],
@@ -34,7 +102,26 @@ const CONTRACT_ABI = [
     "type": "function"
   },
   {
-    "inputs": [{ "internalType": "uint256", "name": "points", "type": "uint256" }],
+    "inputs": [],
+    "name": "owner",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "points",
+        "type": "uint256"
+      }
+    ],
     "name": "redeemPoints",
     "outputs": [],
     "stateMutability": "nonpayable",
@@ -42,8 +129,16 @@ const CONTRACT_ABI = [
   },
   {
     "inputs": [
-      { "internalType": "address", "name": "to", "type": "address" },
-      { "internalType": "uint256", "name": "points", "type": "uint256" }
+      {
+        "internalType": "address",
+        "name": "to",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "points",
+        "type": "uint256"
+      }
     ],
     "name": "transferPoints",
     "outputs": [],
@@ -51,9 +146,40 @@ const CONTRACT_ABI = [
     "type": "function"
   },
   {
-    "inputs": [{ "internalType": "address", "name": "user", "type": "address" }],
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "name": "users",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "points",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "user",
+        "type": "address"
+      }
+    ],
     "name": "viewPoints",
-    "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }],
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
     "stateMutability": "view",
     "type": "function"
   }
@@ -69,9 +195,21 @@ function App() {
   const [issueAddress, setIssueAddress] = useState('');
   const [issueAmount, setIssueAmount] = useState(0);
   const [isOwnerOrServiceProvider, setIsOwnerOrServiceProvider] = useState(false);
+  const [networkError, setNetworkError] = useState('');
 
   useEffect(() => {
     checkWalletIsConnected();
+
+    // Listen for network changes
+    if (window.ethereum) {
+      window.ethereum.on('chainChanged', handleChainChanged);
+    }
+
+    return () => {
+      if (window.ethereum) {
+        window.ethereum.removeListener('chainChanged', handleChainChanged);
+      }
+    };
   }, []);
 
   useEffect(() => {
@@ -80,6 +218,10 @@ function App() {
       checkIfOwner();
     }
   }, [currentAccount, loyaltyContract]);
+
+  const handleChainChanged = () => {
+    window.location.reload();
+  };
 
   const checkWalletIsConnected = async () => {
     const { ethereum } = window;
@@ -97,9 +239,61 @@ function App() {
       const account = accounts[0];
       console.log('Found an authorized account:', account);
       setCurrentAccount(account);
-      await initializeContract();
+
+      // Check network
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const network = await provider.getNetwork();
+      if (network.chainId !== 17000) {
+        setNetworkError('Please connect to the Holesky testnet');
+        await switchToHolesky();
+        return;
+      } else {
+        setNetworkError('');
+        await initializeContract();
+      }
     } else {
       console.log('No authorized account found');
+    }
+  };
+
+  const switchToHolesky = async () => {
+    try {
+      await window.ethereum.request({
+        method: 'wallet_switchEthereumChain',
+        params: [{ chainId: '0x4268' }], // Hexadecimal for 17000
+      });
+      setNetworkError('');
+      await initializeContract();
+    } catch (switchError) {
+      console.error(switchError);
+      if (switchError.code === 4902) {
+        // Chain not added, let's add it
+        try {
+          await window.ethereum.request({
+            method: 'wallet_addEthereumChain',
+            params: [
+              {
+                chainId: '0x4268', // Hexadecimal for 17000
+                chainName: 'Holesky Testnet',
+                nativeCurrency: {
+                  name: 'Holesky ETH',
+                  symbol: 'ETH',
+                  decimals: 18,
+                },
+                rpcUrls: ['https://holesky.infura.io/v3/YOUR_INFURA_PROJECT_ID'],
+                blockExplorerUrls: ['https://holesky.etherscan.io'],
+              },
+            ],
+          });
+          setNetworkError('');
+          await initializeContract();
+        } catch (addError) {
+          console.error(addError);
+          alert('Failed to add Holesky testnet to MetaMask.');
+        }
+      } else {
+        alert('Failed to switch to the Holesky testnet.');
+      }
     }
   };
 
@@ -108,13 +302,25 @@ function App() {
 
     if (!ethereum) {
       alert('Please install MetaMask!');
+      return;
     }
 
     try {
       const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
       console.log('Found an account! Address:', accounts[0]);
       setCurrentAccount(accounts[0]);
-      await initializeContract();
+
+      // Check network
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const network = await provider.getNetwork();
+      if (network.chainId !== 17000) {
+        setNetworkError('Please connect to the Holesky testnet');
+        await switchToHolesky();
+        return;
+      } else {
+        setNetworkError('');
+        await initializeContract();
+      }
     } catch (err) {
       console.log(err);
     }
@@ -151,11 +357,11 @@ function App() {
     if (userPoints >= redeemAmount) {
       try {
         const tx = await loyaltyContract.redeemPoints(redeemAmount, {
-          gasLimit: 500000 
+          gasLimit: 500000,
         });
         await tx.wait();
         alert('Points redeemed!');
-        getUserPoints(); 
+        getUserPoints();
       } catch (err) {
         console.log(err);
         alert('Failed to redeem points.');
@@ -169,11 +375,11 @@ function App() {
     if (userPoints >= transferAmount) {
       try {
         const tx = await loyaltyContract.transferPoints(transferAddress, transferAmount, {
-          gasLimit: 500000 
-        }); // 修复了语法错误，关闭花括号
+          gasLimit: 500000,
+        });
         await tx.wait();
         alert('Points transferred!');
-        getUserPoints(); 
+        getUserPoints();
       } catch (err) {
         console.log(err);
         alert('Failed to transfer points.');
@@ -186,7 +392,7 @@ function App() {
   const issuePointsHandler = async () => {
     try {
       const tx = await loyaltyContract.issuePoints(issueAddress, issueAmount, currentAccount, {
-        gasLimit: 500000
+        gasLimit: 500000,
       });
       await tx.wait();
       alert('Points issued!');
@@ -197,11 +403,11 @@ function App() {
   };
 
   const connectWalletButton = () => {
-    return (
-      <button onClick={connectWalletHandler}>
-        Connect Wallet
-      </button>
-    )
+    return <button onClick={connectWalletHandler}>Connect Wallet</button>;
+  };
+
+  const networkErrorMessage = () => {
+    return <div style={{ color: 'red' }}>{networkError}</div>;
   };
 
   const userInterface = () => {
@@ -211,33 +417,53 @@ function App() {
 
         <div>
           <h3>Redeem Points</h3>
-          <input type="number" placeholder="Amount" onChange={(e) => setRedeemAmount(parseInt(e.target.value))} />
+          <input
+            type="number"
+            placeholder="Amount"
+            onChange={(e) => setRedeemAmount(parseInt(e.target.value))}
+          />
           <button onClick={redeemPointsHandler}>Redeem</button>
         </div>
 
         <div>
           <h3>Transfer Points</h3>
-          <input type="text" placeholder="Recipient Address" onChange={(e) => setTransferAddress(e.target.value)} />
-          <input type="number" placeholder="Amount" onChange={(e) => setTransferAmount(parseInt(e.target.value))} />
+          <input
+            type="text"
+            placeholder="Recipient Address"
+            onChange={(e) => setTransferAddress(e.target.value)}
+          />
+          <input
+            type="number"
+            placeholder="Amount"
+            onChange={(e) => setTransferAmount(parseInt(e.target.value))}
+          />
           <button onClick={transferPointsHandler}>Transfer</button>
         </div>
 
         {isOwnerOrServiceProvider && (
           <div>
             <h3>Issue Points</h3>
-            <input type="text" placeholder="User Address" onChange={(e) => setIssueAddress(e.target.value)} />
-            <input type="number" placeholder="Amount" onChange={(e) => setIssueAmount(parseInt(e.target.value))} />
+            <input
+              type="text"
+              placeholder="User Address"
+              onChange={(e) => setIssueAddress(e.target.value)}
+            />
+            <input
+              type="number"
+              placeholder="Amount"
+              onChange={(e) => setIssueAmount(parseInt(e.target.value))}
+            />
             <button onClick={issuePointsHandler}>Issue</button>
           </div>
         )}
-
       </div>
-    )
+    );
   };
 
   return (
     <div>
       <h1>Loyalty Program</h1>
+      {networkError && networkErrorMessage()}
       {currentAccount ? userInterface() : connectWalletButton()}
     </div>
   );
